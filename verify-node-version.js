bccb5fb208c2cef46734c3da25d44959307c9be0
prisis <d.bannert@anolilab.de>
2018-12-05 18:45:16 +0100

    fixed travis ci

--- verify-node-version.js
+++ verify-node-version.js
@@ -2,7 +2,7 @@ const requiredVersion = require("fs")
   .readFileSync(".nvmrc", { encoding: "utf8" })
   .trim();
 
-if (process.version.split(".")[0] !== requiredVersion.split(".")[0]) {
+if ((! ('TRAVIS' in process.env && 'CI' in process.env) || process.env.SKIP_CHECK !== undefined) && process.version.split(".")[0] !== requiredVersion.split(".")[0]) {
   console.error(
     `[!] This project requires Node.js ${requiredVersion}, current version is ${
       process.version
