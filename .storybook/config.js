12b0491c84c5a99c178d52a66a2d101fd650dd1d
prisis <d.bannert@anolilab.de>
2018-12-05 15:00:45 +0100

    updated storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,91 +1,109 @@
 import * as storybook from '@storybook/react';
-import { withOptions } from '@storybook/addon-options';
-import { checkA11y } from '@storybook/addon-a11y';
+import {withOptions} from '@storybook/addon-options';
+import {checkA11y} from '@storybook/addon-a11y';
+import {withThemes} from 'storybook-styled-components'
+import {storybookGrowCssTheme} from "./theme";
+import {withInfo} from "@storybook/addon-info";
 
 // Option defaults:
-withOptions({
-  /**
-   * name to display in the top left corner
-   * @type {String}
-   */
-  name: 'GrowCss',
+storybook.addDecorator(
+  withOptions({
+    /**
+     * name to display in the top left corner
+     * @type {String}
+     */
+    name: 'GrowCss',
 
-  /**
-   * URL for name in top left corner to link to
-   * @type {String}
-   */
-  url: '#',
+    /**
+     * URL for name in top left corner to link to
+     * @type {String}
+     */
+    url: '#',
 
-  /**
-   * show story component as full screen
-   * @type {Boolean}
-   */
-  goFullScreen: false,
+    /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    goFullScreen: false,
 
-  /**
-   * display panel that shows a list of stories
-   * @type {Boolean}
-   */
-  showStoriesPanel: true,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showStoriesPanel: true,
 
-  /**
-   * display panel that shows addon configurations
-   * @type {Boolean}
-   */
-  showAddonPanel: false,
+    /**
+     * display panel that shows addon configurations
+     * @type {Boolean}
+     */
+    showAddonPanel: true,
 
-  /**
-   * display floating search box to search through stories
-   * @type {Boolean}
-   */
-  showSearchBox: false,
+    /**
+     * display floating search box to search through stories
+     * @type {Boolean}
+     */
+    showSearchBox: false,
 
-  /**
-   * show addon panel as a vertical panel on the right
-   * @type {Boolean}
-   */
-  addonPanelInRight: false,
+    /**
+     * show addon panel as a vertical panel on the right
+     * @type {Boolean}
+     */
+    addonPanelInRight: true,
 
-  /**
-   * sorts stories
-   * @type {Boolean}
-   */
-  sortStoriesByKind: false,
+    /**
+     * sorts stories
+     * @type {Boolean}
+     */
+    sortStoriesByKind: false,
 
-  /**
-   * regex for finding the hierarchy separator
-   * @example:
-   *   null - turn off hierarchy
-   *   /\// - split by `/`
-   *   /\./ - split by `.`
-   *   /\/|\./ - split by `/` or `.`
-   * @type {Regex}
-   */
-  hierarchySeparator: null,
+    /**
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
+     */
+    hierarchySeparator: null,
 
-  /**
-   * regex for finding the hierarchy root separator
-   * @example:
-   *   null - turn off mulitple hierarchy roots
-   *   /\|/ - split by `|`
-   * @type {Regex}
-   */
-  hierarchyRootSeparator: null,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off mulitple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator: null,
 
-  /**
-   * sidebar tree animations
-   * @type {Boolean}
-   */
-  sidebarAnimations: true,
+    /**
+     * sidebar tree animations
+     * @type {Boolean}
+     */
+    sidebarAnimations: true,
 
-  /**
-   * id to select an addon panel
-   * @type {String}
-   */
-  selectedAddonPanel: undefined, // The order of addons in the "Addon panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
-});
+    /**
+     * id to select an addon panel
+     * @type {String}
+     */
+    selectedAddonPanel: undefined, // The order of addons in the "Addon panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
+
+    /**
+     * @type {object}
+     */
+    theme: storybookGrowCssTheme,
+  })
+);
 
 storybook.addDecorator(checkA11y);
+storybook.addDecorator(withInfo({
+  inline: true,
+  source: false
+}));
+storybook.addDecorator(
+  withThemes({
+  })
+);
 
 // automatically import all files ending with *.stories.js
 const req = require.context('..', true, /packages\/((?!node_modules).)*__stories__\/[^\/]+\.(js|tsx)+$/);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,11 +3,21 @@ const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
 module.exports = (baseConfig, env, defaultConfig) => {
   defaultConfig.module.rules.push(
     {
-      test: /\.(js|ts|tsx)$/,
+      test: /\.(js|ts|tsx|jsx)$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: { cacheDirectory: true },
-    }
+    },
+    {
+      test: /\.stories\.(js|ts|tsx|jsx)$/,
+      loaders: [
+        {
+          loader: require.resolve('@storybook/addon-storysource/loader'),
+          options: { parser: 'typescript' }
+        }
+      ],
+      enforce: 'pre',
+    },
   );
 
   defaultConfig.plugins.push(new TSDocgenPlugin());
