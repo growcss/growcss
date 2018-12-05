972b5e3f90635822d8c1a6b6d532557f61b4ece7
prisis <d.bannert@anolilab.de>
2018-12-05 18:55:06 +0100

    fixed node version check

--- verify-node-version.js
+++ verify-node-version.js
@@ -2,12 +2,16 @@ const requiredVersion = require("fs")
   .readFileSync(".nvmrc", { encoding: "utf8" })
   .trim();
 
-if ((! ('TRAVIS' in process.env && 'CI' in process.env) || process.env.SKIP_CHECK !== undefined) && process.version.split(".")[0] !== requiredVersion.split(".")[0]) {
+if (('TRAVIS' in process.env && 'CI' in process.env) || process.env.SKIP_CHECK !== undefined) {
+  process.exit(0);
+}
+
+if (process.version.split(".")[0] !== requiredVersion.split(".")[0]) {
   console.error(
     `[!] This project requires Node.js ${requiredVersion}, current version is ${
       process.version
       }`
   );
 
-  process.exit(1)
+  process.exit(1);
 }
