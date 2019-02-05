19eba5c2f84981caf2e4222d2a94ab75f59c79d2
prisis <d.bannert@anolilab.de>
2019-02-05 13:20:11 +0100

    style(theme): cs fixes

--- packages/core/theme/bin/create-normalize-file.js
+++ packages/core/theme/bin/create-normalize-file.js
@@ -4,16 +4,22 @@ const path = require('path');
 
 const cleanRegexp = /\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$/gm;
 
-const normalizePackage = JSON.parse(fs.readFileSync('./node_modules/normalize.css/package.json', 'utf8'));
+const normalizePackage = JSON.parse(
+  fs.readFileSync('./node_modules/normalize.css/package.json', 'utf8'),
+);
 const normalizeMain = path.join('./node_modules/normalize.css', normalizePackage.main);
 const normalizeContent = fs.readFileSync(normalizeMain, 'utf8');
 
-const cleanedContent = normalizeContent.replace(cleanRegexp, '').replace(/^\s*\n/gm, '').replace(/\s+$/gm, '');
+const cleanedContent = normalizeContent
+  .replace(cleanRegexp, '')
+  .replace(/^\s*\n/gm, '')
+  .replace(/\s+$/gm, '')
+  .replace(/\"/gm, '\'');
 
 const resultContent = `import { css } from 'styled-components';
 
 export default css\`
-${cleanedContent}
+  ${cleanedContent}
 \`;
 `;
 
