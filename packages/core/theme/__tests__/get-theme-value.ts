160d6b5f0712f316043a425037d110fb224c23a7
Daniel Bannert <d.bannert@anolilab.de>
2019-02-11 00:17:55 +0100

    style(theme): cs fixes

--- packages/core/theme/__tests__/get-theme-value.ts
+++ packages/core/theme/__tests__/get-theme-value.ts
@@ -1,15 +1,13 @@
 import { getThemeValue } from '../src/utils/get-theme-value';
 
 test('string argument', () => {
-  expect(getThemeValue('color')({  })).toBeUndefined();
+  expect(getThemeValue('color')({})).toBeUndefined();
   expect(getThemeValue('color')({ color: 'red' })).toBe('red');
 });
 
 test('deep string argument', () => {
   expect(getThemeValue('color.primary')({ color: {} })).toBeUndefined();
-  expect(getThemeValue('color.primary')({ color: { primary: 'red' } })).toBe(
-    'red',
-  );
+  expect(getThemeValue('color.primary')({ color: { primary: 'red' } })).toBe('red');
 });
 
 test('defaultValue', () => {
