275bc55d0636dc698fac158c85bebf45dbc4c318
prisis <d.bannert@anolilab.de>
2019-02-12 17:06:51 +0100

    feat(eslint-config): added jsdoc lint

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,5 +2,4 @@ import '@storybook/addon-a11y/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-options/register';
 import 'storybook-addon-specifications/register';
-import 'storybook-styled-components/register'
 import 'storybook-chrome-screenshot/register';
