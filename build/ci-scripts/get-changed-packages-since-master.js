b7255b930fa6333f72199e0c058459d07b80e86e
prisis <d.bannert@anolilab.de>
2019-02-13 09:55:36 +0100

    build: update package-lock
    build: added check for files in test folder

--- build/ci-scripts/get-changed-packages-since-master.js
+++ build/ci-scripts/get-changed-packages-since-master.js
@@ -3,7 +3,7 @@ const { getChangedPackagesSinceMaster } = require('../utils/packages');
 /**
  * NOTE: This prints the list of changed packages that would be the subjects of the next lerna version or lerna publish execution.
  * It will print them all out as a json array of relative paths
- * i.e: $ node build/ci-scripts/get.changed.packages.since.master.js
+ * i.e: $ node build/ci-scripts/get-changed-packages-since-master.js
  *        ["packages/core/avatar", "packages/core/badge"]
  */
 (async () => {
