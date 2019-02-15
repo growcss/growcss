5a7af4dfbe831b6c1becd15c7e43d3e814465ab1
Daniel Bannert <prisis@users.noreply.github.com>
2019-02-15 18:35:12 +0100

    build: Updated semantic git version tagging (#135)
    * build: fix git version
    
    * build: update package.json and lock
    
    * build: more changes to the semantic-release
    
    * build: fix test

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,6 @@
 const hooks = require('semantic-release-monorepo-hooks');
 const output = hooks();
+const PACKAGE_NAME = process.env.LERNA_PACKAGE_NAME || process.env.npm_package_name;
 
 module.exports = {
   branches: [
@@ -10,10 +11,11 @@ module.exports = {
     { name: 'beta', prerelease: true },
     { name: 'alpha', prerelease: true }
   ],
-  tagFormat: 'v${version}',
+  tagFormat: `${PACKAGE_NAME}@\\${version}`,
   prepare: [
     '@semantic-release/changelog',
     '@semantic-release/npm',
+    'upgrade-dependents/semantic-release',
     {
       'path': '@semantic-release/git',
       'message': 'chore(' + output.package + '): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
